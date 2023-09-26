b7bb19080fc27679748223f113ddc175d0b3d02b
Egg-Man-Real <104573822+Egg-Man-Real@users.noreply.github.com>
2023-09-25 21:27:21 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -34,7 +34,7 @@ const Moyai: Plugin = {
       }
 
       let attempt = 0
-      const attempts = 1
+      const attempts = 3
 
       const lateStart = () => {
          try {
@@ -75,7 +75,7 @@ const Moyai: Plugin = {
                      if (paused) setPaused(false)
                      // Secret!
                      args[0].message.content = args[0].message.content.replace("*‍*", " 🗿")
-                     set(Manifest.name, 'moyaiCounter', Number(get(Manifest.name, 'moyaiCounter', 0)) + 1)
+                    
                   }
                })
 
@@ -86,7 +86,7 @@ const Moyai: Plugin = {
 
                      // Secret!
                      args[0].message.content = args[0].message.content.replace("*‍*", " 🗿")
-                     set(Manifest.name, 'moyaiCounter', Number(get(Manifest.name, 'moyaiCounter', 0)) + 1)
+                    
                   }
                })
 
@@ -94,7 +94,7 @@ const Moyai: Plugin = {
                   if (args[0].channelId === channelId && isBoomWorthy(args[0].emoji.name)) {
                      vid.seek(0)
                      if (paused) setPaused(false)
-                     set(Manifest.name, 'moyaiCounter', Number(get(Manifest.name, 'moyaiCounter', 0)) + 1)
+                
                   }
                   
                })
